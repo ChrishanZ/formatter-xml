f95593944cecf38931baf25a8a0702523b79edea
ChrishanZ <chrisduttoo@gmail.com>
2025-08-26 18:47:59 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen relative">
+      <p className="absolute top-2 left-2 text-xs">Boilerplate with React, Tailwind, Typescript</p>
+      <h1 className="text-4xl font-bold">Hello, World !</h1>
+    </div>
+  )
+}
+
+export default App
